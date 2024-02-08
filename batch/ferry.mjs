25c7184e89101e4a381671a34cac0c8cedb2a0b8
Matt Wood <mza@hey.com>
2024-02-07 21:33:42 -0800

    Solved the time converstion problem for negative epochs.

--- batch/ferry.mjs
+++ batch/ferry.mjs
@@ -1,22 +1,28 @@
-import { fetchJSONFromURL } from '../lib/fetch.mjs';
-import { parseSailings } from './sailings.mjs';
+import WSF from '../wsf.mjs';
 
 const apiaccesscode = "333bb279-f90c-491b-88e3-f82cddb6599e";
-const tripDate = "2024-02-07"
-const prefix = "https://www.wsdot.wa.gov/Ferries/API/Schedule/rest";
-const suffix = `apiaccesscode=${apiaccesscode}`;
+const wsf = new WSF(apiaccesscode);
 
-// Last updated:
+wsf.fetchCacheDate().then(data => {
+  console.log(data);
+});
 
-// var action = "schedroutes";
+wsf.fetchSailings(2359).then(data => {
+  // console.log(JSON.stringify(data.sailings, null, 2));
+});
 
-var action = "sailings/2359";
-var url = `${prefix}/${action}?${suffix}`;
 
-fetchJSONFromURL(url)
-  .then(data => 
-  {
-    // console.log(data[0]['Journs'][0]['TerminalTimes'][0]['Time']);
-    parseSailings(data);
-  })
-  .catch(error => console.error(error));
+// // Last updated:
+
+// // var action = "schedroutes";
+
+// var action = "sailings/2359";
+// var url = `${prefix}/${action}?${suffix}`;
+
+// fetchJSONFromURL(url)
+//   .then(data => 
+//   {
+//     // console.log(data[0]['Journs'][0]['TerminalTimes'][0]['Time']);
+//     parseSailings(data);
+//   })
+//   .catch(error => console.error(error));
--- wsf.mjs
+++ wsf.mjs
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { parseAndConvertTimestamp } from './lib/dateConverter.mjs';
-import { fetchJSONFromURL } from './lib/fetch.mjs';
+import { parseAndConvertTimestamp, convertToTime } from './lib/dateConverter.mjs';
 
 class WSF {
   constructor(api_key) {
@@ -15,11 +14,40 @@ class WSF {
   async fetchCacheDate() {
     var action = "cacheflushdate";
     const response = await this.fetch(action);
+
     this.data = {
       last_updated: parseAndConvertTimestamp(response)
     };
+
     return this.data;
   }
+
+  async fetchSailings(routeId) {
+    var action = "sailings/" + routeId;
+    const response = await this.fetch(action);
+
+    this.parseSailings(response);
+
+    this.data = {
+      sailings: response
+    };
+    
+    return this.data;
+  }
+
+  parseSailings(schedules) {
+    schedules.forEach((schedule, index) => {
+        console.log(`Schedule ID: ${schedule.ScheduleID}, Route ID: ${schedule.RouteID}, Description: ${schedule.SailingDescription}`);
+        schedule.Journs.forEach(journey => {
+            console.log(journey.VesselName);
+            journey.TerminalTimes.forEach(terminalTime => {
+                if (!terminalTime.IsNA) {
+                  console.table([ terminalTime.TerminalDescription, convertToTime(terminalTime.Time)]);
+                }
+            });
+        });
+    });
+  };
 
   async fetch(action) {
 
@@ -27,7 +55,7 @@ class WSF {
     const suffix = `apiaccesscode=${this.api_key}`;
 
     var url = `${prefix}/${action}?${suffix}`;
-    console.log(url);
+
     try {
       const response = await axios.get(url);
       return response.data;
